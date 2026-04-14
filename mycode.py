ba0436c3c98f1f7103d99590f9c93678b50e8446
Ashok <reddyashok075@gmail.com>
2026-04-14 16:09:31 +0530

    added new record

--- mycode.py
+++ mycode.py
@@ -15,8 +15,6 @@ new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
 
-
-
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir,exist_ok=True)
